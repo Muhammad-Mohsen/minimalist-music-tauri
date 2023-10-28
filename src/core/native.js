b1d3363450b462d6a94a545bcdd8b9c58210435e
Muhammad Mohsen <muhammad.mohsen@innuva.com>
2023-10-28 16:58:42 +0200

    v1.0 YAY!

--- src/core/native.js
+++ src/core/native.js
@@ -5,7 +5,6 @@ var Native = (() => {
 	const fs = window.__TAURI__.fs;
 	const dialog = window.__TAURI__.dialog;
 	const convertFileSrc = window.__TAURI__.tauri.convertFileSrc;
-	const shortcut = window.__TAURI__.globalShortcut;
 
 	// FS
 	const PATH_SEPARATOR = '\\';
@@ -71,23 +70,6 @@ var Native = (() => {
 		appWindow.setTitle(title);
 	}
 
-	function onWindowFocus(callback) {
-		appWindow.listen("tauri://focus", ({ event, payload }) => callback(event, payload))
-	}
-	function onWindowBlur(callback) {
-		appWindow.listen("tauri://blur", ({ event, payload }) => callback(event, payload))
-	}
-
-	// SHORTCUTS
-	async function registerShortcut(keys, handler) {
-		// register it on launch
-		if (!(await shortcut.isRegistered(keys))) shortcut.register(keys, handler);
-
-		// handle window events
-		onWindowBlur(async () => { if (await shortcut.isRegistered(keys)) shortcut.unregister(keys) });
-		onWindowFocus(async () => { if (!(await shortcut.isRegistered(keys))) shortcut.register(keys, handler) });
-	}
-
 	return {
 		FS: {
 			PATH_SEPARATOR,
@@ -111,12 +93,6 @@ var Native = (() => {
 			minimize: minimizeWindow,
 			resize: resizeWindow,
 			size: windowSize,
-			onFocus: onWindowFocus,
-			onBlur: onWindowBlur,
-		},
-
-		shortcut: {
-			register: registerShortcut,
 		}
 	}
 
--- src/core/shortcuts.js
+++ src/core/shortcuts.js
@@ -2,12 +2,22 @@
 
 	const SELF = EventBus.target.KEYBOARD;
 
-	Native.shortcut.register('Space', () => EventBus.dispatch({ type: EventBus.type.PLAY_PAUSE, target: SELF }));
-	Native.shortcut.register('0', () => EventBus.dispatch({ type: EventBus.type.FROM_THE_TOP, target: SELF }));
-	// Native.shortcut.register('Numpad0', () => EventBus.dispatch({ type: EventBus.type.FROM_THE_TOP, target: SELF })); // DOESN'T WORK
-	Native.shortcut.register('ArrowLeft', () => EventBus.dispatch({ type: EventBus.type.RW, target: SELF }));
-	Native.shortcut.register('ArrowRight', () => EventBus.dispatch({ type: EventBus.type.FF, target: SELF }));
-	Native.shortcut.register('Ctrl+ArrowLeft', () => EventBus.dispatch({ type: EventBus.type.PLAY_PREV, target: SELF }));
-	Native.shortcut.register('Ctrl+ArrowRight', () => EventBus.dispatch({ type: EventBus.type.PLAY_NEXT, target: SELF }));
+	window.onkeyup = (event) => {
+		if (event.target.tagName.toUpperCase() == 'INPUT') return; // ignore key presses in the search box
+
+		event.preventDefault();
+
+		when(event.code)
+			.is('Space', () => EventBus.dispatch({ type: EventBus.type.PLAY_PAUSE, target: SELF }))
+			.is('Numpad0', () => EventBus.dispatch({ type: EventBus.type.FROM_THE_TOP, target: SELF }))
+			.is('ArrowLeft', () => {
+				if (event.ctrlKey) EventBus.dispatch({ type: EventBus.type.PLAY_PREV, target: SELF })
+				else EventBus.dispatch({ type: EventBus.type.RW, target: SELF })
+			})
+			.is('ArrowRight', () => {
+				if (event.ctrlKey) EventBus.dispatch({ type: EventBus.type.PLAY_NEXT, target: SELF })
+				else EventBus.dispatch({ type: EventBus.type.FF, target: SELF })
+			});
+	}
 
 })();
